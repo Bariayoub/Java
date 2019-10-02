edbbbca9c349760ab03381b9b2e3f90472e02765
Bariayoub <>
2019-10-02 09:36:33 +0100

    java-v1.1

---
+++ intro/src/animal/animal.java
@@ -0,0 +1,76 @@
+package animal;
+
+public class animal {
+	
+	private static int nbranimal=0;
+	private String nom;
+	private int age;
+	private int poid;
+	private double speed;
+	private int id;
+	
+	public animal() {
+		this.nbranimal++;
+	}
+
+	public animal(String nom, int age, int poid, double speed, int id) {
+		this.nbranimal++;
+		this.nom = nom;
+		this.age = age;
+		this.poid = poid;
+		this.speed = speed;
+		this.id = id;
+	}
+
+	public void afficher(){
+		System.out.println("id : "+getId()+" |nom : "+getNom()+" |age : "+getAge()+" |poid : "+getPoid()+" | speed :"+getSpeed()+"| nombre de hmar : "+nbranimal);
+	}
+
+	public static int getNbranimal() {
+		return nbranimal;
+	}
+
+	public static void setNbranimal(int nbranimal) {
+		animal.nbranimal = nbranimal;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getPoid() {
+		return poid;
+	}
+
+	public void setPoid(int poid) {
+		this.poid = poid;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
---
+++ intro/src/animal/testAnimal.java
@@ -0,0 +1,21 @@
+package animal;
+
+public class testAnimal extends animal {
+
+	public testAnimal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+		testAnimal an1 = new testAnimal();
+		an1.setAge(12);
+		an1.setNom("anne");
+		an1.setPoid(156);
+		an1.setSpeed(120);
+		an1.setId(animal.getNbranimal());
+		an1.afficher();
+		new animal("homar",20,150,60,3).afficher();
+	}
+
+}
