72e0bf0f32f45522787fe6d44303062b37a36541
Bariayoub <>
2019-10-30 10:17:44 +0000

    java 30/10/2019

--- intro/src/InputOutput/main.java
+++ intro/src/InputOutput/main.java
@@ -27,23 +27,31 @@ public class main {
             e.printStackTrace();
         }
         */
-
-
-
+        
         File f = new File("filename.txt");
+        File f2 = new File("filename2.txt");
         FileInputStream fileInputStream = null;
+        FileOutputStream fileOutputStream = null;
         try {
+            fileOutputStream = new FileOutputStream(f2);
             fileInputStream =new FileInputStream(f);
             byte[] buf=  new  byte[10];
             int n = 0;
             while ((n=fileInputStream.read(buf))>=0){
-                for (int i=0;i<n;i++)
-                System.out.print((char)buf[i]);
+                for (int i=0;i<n;i++) {
+                    System.out.print((char) buf[i]);
+                    fileOutputStream.write((char) buf[i]);
+                }
             }
-                fileInputStream.close();
+            fileOutputStream.close();
+            fileInputStream.close();
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
+
+
+
+
 
 
     }
